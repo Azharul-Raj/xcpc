12f14d708c3781b1f682f44771289d58ff6e868d
Azharul-Raj <azharulislam.raj512@gmail.com>
2024-04-09 10:54:15 +0600

    solve problem B

---
+++ week4/day1/B.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  raj_001
+ *    created: 2024-04-09 09:59:06
+ **/
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long int
+using namespace std;
+
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   int t;cin>>t;
+   while(t--){
+    int n;cin>>n;
+    for(int i=n;i>0;i--)cout<<i<<" ";
+    cout<<nl;
+   }
+   return 0;
+}
