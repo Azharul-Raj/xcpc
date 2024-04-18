bcaa3f146ba82497bd6dd7b8d7b50ff5f1c10d43
Azharul-Raj <azharulislam.raj512@gmail.com>
2024-04-18 19:49:42 +0600

    solve boxes problem

---
+++ week4/day4/N.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  raj_001
+ *    created: 2024-04-18 15:54:40
+ **/
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long int
+using namespace std;
+
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   int t=1;
+   cin>>t;
+   while(t--){
+      int n;cin>>n;
+      cout<<n<<" "<<n-1<<nl;
+   }
+   return 0;
+}
