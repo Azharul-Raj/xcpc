f4ac7dbf3016db5e91edbfbedb45dc5d6f4bdcd3
Azharul-Raj <azharulislam.raj512@gmail.com>
2024-08-27 14:38:34 +0600

    solve problem E

--- week17/day2/E.cpp
+++ week17/day2/E.cpp
@@ -1,70 +1,130 @@
 /**
  *    author:  raj_001
- *    created: 2024-08-17 10:49:24
+ *    created: 2024-08-27 13:00:58
  **/
 #include <bits/stdc++.h>
-#define nl '\n'
-#define int long long int
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+ll lcm(ll a, ll b)
+{
+    return (a / __gcd(a, b)) * b;
+}
+#define nl '\n'
 #define INF 1e18
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
 #define pb push_back
 #define ppb pop_back
+#define rf(i, a, b) for (int i = a; i < (b); i++)
+#define fr(i, a) for (int i = 0; i < (a); i++)
+#define frr(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define ys cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// Templete from Geothermal
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ", ";
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '[';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), __print(i);
+    cerr << "]";
+}
+void _print() { cerr << "\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifdef DEBUG
+#define dbg(x...)                                                               \
+    cerr << /*"\e[91m" << __func__ << ":" << __LINE__ << */ " " << #x << " = "; \
+    _print(x);                                                                  \
+    // cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
 
 void solve()
 {
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(n), pref(n);
-    for (int &v : a)
-        cin >> v;
-    //
-    int f = a.front(), l = a.back();
-    if (f == l)
+    string a, b, s, t;
+    cin >> a >> b;
+    s = a, t = b;
+    while (sz(a) != sz(b))
     {
-        pref[0] = 1;
-    }
-    // Prefix sum section
-    for (int i = 1; i < n; i++)
-    {
-        pref[i] = pref[i - 1] + (a[i] == l);
-    }
-    // prefix sum section end
-
-    if (l == f)
-    {
-        if (pref[n - 1] >= k)
-            cout << "YES" << nl;
+        if (sz(a) < sz(b))
+        {
+            a += s;
+        }
         else
-            cout << "NO" << nl;
-        return;
-    }
-    //
-    int valCount = 0, idx = -1;
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] == f)
         {
-            valCount++;
-        }
-        if (valCount == k)
-        {
-            idx = i;
-            break;
+            b += t;
         }
     }
-    int lastValCount = pref[n - 1] - pref[idx];
-    if (idx == -1 || lastValCount < k)
-        cout << "NO" << nl;
+    if (a == b)
+        cout << a << nl;
     else
-        cout << "YES" << nl;
+        cout << -1 << nl;
 }
 
 int32_t main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0);
+    cout.tie(0);
     int t = 1;
     cin >> t;
     while (t--)
